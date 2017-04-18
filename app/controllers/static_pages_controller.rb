5c9e67e2a3d18ea7eda05389d140c9a267ff8170
Humberto Ramos <humbertocrv@hotmail.com>
2017-04-17 19:09:32 -0500

    usuario funcionando y consola basica

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,19 @@
 class StaticPagesController < ApplicationController
+
+  before_action :authenticate_user!
+
   def home
+
   end
+
+  def console
+
+  end
+
+  def movements
+
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,16 @@ Rails.application.routes.draw do
 
   get 'static_pages/home'
   root 'static_pages#home'
+
+  get 'static_pages/console'
+  get 'static_pages/movements'
+
+  get 'static_pages/reforms'
+  get 'static_pages/profile'
+  
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   
